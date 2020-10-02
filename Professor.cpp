88cebcf59306f4e3974fdfec66f761427b06c40c
ahmedrizvi <ahmed.riz117@gmail.com>
2020-10-01 20:10:31 -0400

    Add project files.

---
+++ Professor.cpp
@@ -0,0 +1,51 @@
+/**
+ * Project OOP3200-F2020-Lesson4
+ * @author Ahmed Rizvi
+ * @version 1
+ */
+
+
+#include "Professor.h"
+
+/**
+ * Professor implementation
+ */
+
+
+/**
+ * @param first_name
+ * @param last_name
+ * @param age
+ * @param employeeID
+ */
+void Professor::Professor(string first_name, string last_name, float age, string employeeID) {
+
+}
+
+/**
+ * @return string
+ */
+string Professor::getEmployeeID() {
+    return "";
+}
+
+/**
+ * @param value
+ */
+void Professor::setEmployeeID(string value) {
+
+}
+
+/**
+ * @return void
+ */
+void Professor::Teaches() {
+    return;
+}
+
+/**
+ * @return string
+ */
+string Professor::ToString() {
+    return "";
+}
